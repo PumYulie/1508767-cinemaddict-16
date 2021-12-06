1c63a0f8fc295144fcd43bdba786f5dc22c11d5f
PumYulie <yulisakova@gmail.com>
2021-12-07 00:19:54 +0300

    пофиксила самодеятельность

--- src/main.js
+++ src/main.js
@@ -1,33 +1,27 @@
-import {renderSiteMenu} from './view/site-menu.js';
-import {renderCardInList} from './view/card-in-list.js';
 import {renderUserName} from './view/user-name.js';
+import {renderSiteMenu} from './view/site-menu.js';
+import {renderFilmsList} from './view/films-list.js';
+import {renderFilmsListItem} from './view/films-list-item.js';
 import {renderShowMoreButton} from './view/show-more-button.js';
-import {renderPopupWithoutComment} from './view/popup-without-comment.js';
-
-const renderComponent = (container, html, placeToPutHtml) => {
-  container.insertAdjacentHTML(placeToPutHtml, html);
-};
+//import {renderPopupWithoutComment} from './view/popup-without-comment.js';
 
 const FILM_CARDS_COUNT = 5;
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
-const filmCardsContainer = document.createElement('div');
 
-//очень хочу, чтобы карточки построились горищонтально, а они не строятся
-//что сделать?
-filmCardsContainer.setAttribute('display', 'flex');
-filmCardsContainer.setAttribute('justify-content', 'space-between');
+const renderComponent = (container, html, placeToPutHtml) => {
+  container.insertAdjacentHTML(placeToPutHtml, html);
+};
 
 renderComponent(headerElement, renderUserName(), 'beforeend');
 renderComponent(mainElement, renderSiteMenu(), 'beforeend');
+renderComponent(mainElement, renderFilmsList(), 'beforeend');
 
-for (let i = 0; i <= FILM_CARDS_COUNT; i++) {
-  renderComponent(filmCardsContainer, renderCardInList(), 'beforeend');
+const filmsListContainer = document.querySelector('.films-list__container');
+for (let i = 0; i < FILM_CARDS_COUNT; i++) {
+  renderComponent(filmsListContainer, renderFilmsListItem(), 'beforeend');
 }
 
-renderComponent(mainElement, filmCardsContainer.innerHTML, 'beforeend');
 renderComponent(mainElement, renderShowMoreButton(), 'beforeend');
-
-//работает, но пока мешает разборке с выстраиванием карточек фильма горизонтально
 //renderComponent(mainElement, renderPopupWithoutComment(), 'beforeend');
